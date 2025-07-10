f90d48052e6b0867af67f475abe83b2ff8e49f59
Sleyd1 <153521084+Sleyd1@users.noreply.github.com>
2025-07-09 23:29:45 -0300

    revisandoo conteudo

---
+++ JavaR/src/main/java/org/example/LerNumeros.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class LerNumeros {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3,4,5,6,7,8,9,10};
+
+        for (int i : nums){
+            System.out.println(i);
+        }
+    }
+}
