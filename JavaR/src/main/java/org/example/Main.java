f478c0b70e061cb2718a290935a83055098a25d8
Sleyd1 <211575227+WesleyJunio0@users.noreply.github.com>
2025-07-12 13:42:22 -0300

    revisandoo conteudo

---
+++ JavaR/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int num = -10;
+         if (num == 0){
+             System.out.println("numero igual a zero");
+         } else if (num > 0) {
+             System.out.println("numero positivo");
+         }else {
+             System.out.println("numero negativo");
+         }
+
+
+    }
+}
